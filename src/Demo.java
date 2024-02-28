5bf73f3d1db81083949a326491d2eee30556a6fc
Apekshafl <apeksha.gopalrao.fule@gmail.com>
2024-02-28 17:08:43 -0600

    commit

--- src/Demo.java
+++ src/Demo.java
@@ -4,8 +4,12 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("1");
-   System.out.println("2");
-   System.out.println("3");
+   System.out.println("c");
+   System.out.println("a");
+   System.out.println("b");
+   System.out.println("c");
+   System.out.println("d");
+   
 	}
 
 }
